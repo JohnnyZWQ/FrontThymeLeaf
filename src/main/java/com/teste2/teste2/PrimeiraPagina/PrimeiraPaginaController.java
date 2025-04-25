6ad135a1d51b602df18b53a41e76932ec167e2cf
JohnnyZWQ <11232100184@alunos.umc.br>
2025-04-25 18:53:24 -0300

    Adicição do POST "criar"

--- src/main/java/com/teste2/teste2/PrimeiraPagina/PrimeiraPaginaController.java
+++ src/main/java/com/teste2/teste2/PrimeiraPagina/PrimeiraPaginaController.java
@@ -3,6 +3,7 @@ package com.teste2.teste2.PrimeiraPagina;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,4 +16,15 @@ public class PrimeiraPaginaController {
                 "Primeira mensagem do controller");
         return "primeiraPagina";
     }
+
+    @PostMapping("criar")
+    public String create(Criar criar, Model model) {
+        System.out.println("Nome" +criar.nome);
+        System.out.println("Email" + criar.email);
+        System.out.println("Usuario" + criar.usuario);
+        model.addAttribute("criar", criar);
+        return "candidate/info";
+    }
+    record Criar(String nome, String email, String usuario) {}
+}
 }
